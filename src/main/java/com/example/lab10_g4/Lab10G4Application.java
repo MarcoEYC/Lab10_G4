b173d266d603fbe8a213fb8b8061c4c3e005b593
elias <m.yovera@pucp.edu.pe>
2022-11-25 14:09:12 -0500

    inicial

---
+++ src/main/java/com/example/lab10_g4/Lab10G4Application.java
@@ -0,0 +1,13 @@
+package com.example.lab10_g4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab10G4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab10G4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab10_g4/Lab10G4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab10_g4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10G4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
